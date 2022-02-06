050c67c6cc10f87e4be5126cb841cb4744cc7b99
wmaliga <wojciech.maliga@gmail.com>
2022-02-06 16:35:15 +0100

    Dummy data added

---
+++ note-share-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO note (title, data) VALUES ('Lorem ipsum 1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
+INSERT INTO note (title, data) VALUES ('Lorem ipsum 2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
+INSERT INTO note (title, data) VALUES ('Lorem ipsum 3', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.');
