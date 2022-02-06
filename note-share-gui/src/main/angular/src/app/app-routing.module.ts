ec178651a047c83481520b309703b10cc21596f9
wmaliga <wojciech.maliga@gmail.com>
2022-02-06 11:18:23 +0100

    Note Component created

--- note-share-gui/src/main/angular/src/app/app-routing.module.ts
+++ note-share-gui/src/main/angular/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NoteComponent } from "./components/note/note.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: NoteComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
